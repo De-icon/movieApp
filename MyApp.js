f595b777e9d3843edfcbfafedc50f6ad494aec80
isaac john <isaacjohn2028@gmail.com>
2023-07-04 01:56:16 +0100

    added the API

--- MyApp.js
+++ MyApp.js
@@ -1,6 +1,43 @@
 
+window.onload = () => {
+  // fetch upcoming movie
+  
+  fetch('https://moviesdatabase.p.rapidapi.com/titles/x/upcoming',{
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '36d7c9de84msh6dc9183ee581734p1930c4jsnab8ca1caa87a',
+		'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+	}
+})
+  .then(response => response.json())
+  .then(data => {
+    const list = data.results;
 
+    list.map((item) => {
+      const image = item.primaryImage.url
+      const titel = item.originalTitleText.text
+      const day = item.releaseDate.day
+      const month = item.releaseDate.month
+      const year = item.releaseDate.year
 
+      const movies = `<div class="movie">
+        <img src="${image}">
+        <div>
+        <h4>${titel} </h4>
+        <p>${day}-${month}-${year}</p>
+        </div>
+      </div>`
+
+      document.getElementById("up-coming").innerHTML += movies      
+    })
+  })
+  .catch(error => {
+  console.error(error);
+})
+}
+
+ 
+// scroller
 
 function enableHorizontalScroll() {
     const scrollContainers = document.querySelectorAll('.scroll-container');
